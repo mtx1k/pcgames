856783b1ba0d76bb496d04d3f11484269a523bb5
Vitalii <vitaliy.shekhovtsov@gmail.com>
2024-10-24 11:42:05 +0200

    Initial commit

---
+++ src/main/java/dci/pcgames/PcgamesApplication.java
@@ -0,0 +1,13 @@
+package dci.pcgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcgamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcgamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dci/pcgames/PcgamesApplicationTests.java
@@ -0,0 +1,13 @@
+package dci.pcgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcgamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
